c525dd427a1d6e31d337cf71104ce6ddbce2b27c
slashrage <tspaeth78@gmail.com>
2026-04-21 18:52:04 -0500

    Updated form data to push to Web3Forms, then HTTP Post to Make, then to HubSpot for contact and deal creation.

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-$(function() {
-  $('#zammad-feedback-form').ZammadForm({
-    messageTitle: 'Feedback Form',
-    messageSubmit: 'Submit',
-    messageThankYou: 'Thank you for your inquiry (#%s)! We\'ll contact you as soon as possible.',
-    modal: true
-  });
-});
+// This file is no longer used. The contact form is now wired to Web3Forms
+// inline in index.html. Safe to delete in a future cleanup.
